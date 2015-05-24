90094a61a1524441b7d61e3875028cd3af7d3a30
Gray MacGregor <gmacg.uk@gmail.com>
2015-05-24 10:59:44 +0100

    livereload 2

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+
+var EXPRESS_PORT = 4000;
+var EXPRESS_ROOT = __dirname;
+var LIVERELOAD_PORT = 35729;
+var lr;
+
+function startExpress() {
+    
+    var express = require('express');
+    var app = express();
+    app.use(require('connect-livereload')());
+    app.use(express.static(EXPRESS_ROOT));
+    app.listen(EXPRESS_PORT);
+}
+
+function startLivereload() {
+    
+    lr = require('tiny-lr')();
+    lr.listen(LIVERELOAD_PORT);
+}
+
+function notifyLivereload(event) {
+    var fileName = require('path').relative(EXPRESS_ROOT, event.path);
+    lr.changed({
+        body: {
+            files: [fileName]
+        }
+    });
+}
+
+gulp.task('default', function () {
+    startExpress();
+    startLivereload();
+    gulp.watch('app/*.js', notifyLivereload);
+});
