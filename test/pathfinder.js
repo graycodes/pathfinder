d1fdc85f88dac8a953ea8c2ae94929e3b53b11ec
Gray MacGregor <gmacg.uk@gmail.com>
2016-06-07 08:57:04 +0100

    Testing togglewall a bit

--- test/pathfinder.js
+++ test/pathfinder.js
@@ -9,9 +9,21 @@ describe('Pathfinder', function() {
     });
 
     describe('toggleWall', function () {
-      it('should be defined', function () {
-        expect(pathfinder.toggleWall).to.be.defined;
-      })
+        it('should be defined', function () {
+            expect(pathfinder.toggleWall).to.be.defined;
+        });
+        describe('when called with an empty set of walls', function () {
+            it('should return the clicked wall', function () {
+                var clicked = [0,0];
+                expect(pathfinder.toggleWall([], clicked)).to.deep.equal([clicked]);
+            });
+        });
+        describe('when a disabled wall is clicked', function () {
+            it('should return the list of walls, plus the new wall', function () {
+                var clicked = [2,2];
+                expect(pathfinder.toggleWall([[0,0], [0,1]], clicked)).to.deep.equal([[0,0], [0,1], [2,2]]);
+            });
+        })
     })
 
 });
