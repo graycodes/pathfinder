ec665b6d7a5a71c5fc91760dd1233217ff9d0720
Gray MacGregor <gmacg.uk@gmail.com>
2016-06-08 07:57:20 +0100

    Pathfinder findNext

--- app/reducers/pathfinder.js
+++ app/reducers/pathfinder.js
@@ -1,21 +1,69 @@
-module.exports = {
-    toggleWall: function (arr, coords) {
-        var newWalls = [];
+var _ = require('lodash');
 
-        // Remove the wall if it's in arr
-        var filtered = arr.filter(function (pair) {
-            return !(pair[0] === coords[0] && pair[1] === coords[1]);
-        });
+function Pathfinder() {};
 
-        // If the wall wasn't in arr, add it in
-        // Otherwise, return the list minus the wall
-        if (filtered.length === arr.length) {
-            newWalls = newWalls.concat(arr);
-            newWalls = newWalls.concat([coords]);
-            return newWalls;
-        } else {
-            return filtered;
-        }
+var p = Pathfinder.prototype;
+
+p.toggleWall = function (arr, coords) {
+    var newWalls = [];
+
+    // Remove the wall if it's in arr
+    var filtered = arr.filter(function (pair) {
+        return !(pair[0] === coords[0] && pair[1] === coords[1]);
+    });
+
+    // If the wall wasn't in arr, add it in
+    // Otherwise, return the list minus the wall
+    if (filtered.length === arr.length) {
+        newWalls = newWalls.concat(arr);
+        newWalls = newWalls.concat([coords]);
+        return newWalls;
+    } else {
+        return filtered;
     }
-
 }
+
+p.findNext = function (state) {
+    // if the state doesn't have any path, then start. Otherwise loop through the latest path queue;
+    var adjacentTo = state.path.length === 0 ? this.getStart(state.size) : [0,0];
+    return this.getAdjacentFree(adjacentTo, state.size, state.walls, state.path);
+}
+
+p.getStart = function (size) {
+    return [0, Math.floor(size / 2)];
+}
+
+p.getAdjacentFree = function (start, size, walls, path) {
+    var notOffGrid = _.partial(this.notOffGrid, size);
+    var notWall = _.partial(this.notX, walls);
+    var notPath = _.partial(this.notX, path);
+    return _(this.getAdjacent(start))
+            .filter(notWall)
+            .filter(notPath)
+            .filter(notOffGrid)
+            .value();
+}
+
+p.getAdjacent = function (start) {// 0, 1
+    var x = start[0];
+    var y = start[1];
+    return [
+        [x, y - 1],
+        [x + 1, y],
+        [x, y + 1],
+        [x - 1, y]
+    ];
+}
+
+p.notOffGrid = function (size, point) {
+    var x = point[0];
+    var y = point[1];
+    return x >= 0 && x < size && y >= 0 && y < size;
+}
+
+p.notX = function (walls, point) {
+    var intersection = _.intersectionWith(walls, [point], _.isEqual);
+    return intersection.length === 0;
+}
+
+module.exports = new Pathfinder();
--- test/pathfinder.js
+++ test/pathfinder.js
@@ -9,21 +9,81 @@ describe('Pathfinder', function() {
     });
 
     describe('toggleWall', function () {
-        it('should be defined', function () {
-            expect(pathfinder.toggleWall).to.be.defined;
+
+        it('should return the clicked wall when called with an empty set of walls', function () {
+            var clicked = [0,0];
+            expect(pathfinder.toggleWall([], clicked)).to.deep.equal([clicked]);
         });
-        describe('when called with an empty set of walls', function () {
-            it('should return the clicked wall', function () {
-                var clicked = [0,0];
-                expect(pathfinder.toggleWall([], clicked)).to.deep.equal([clicked]);
-            });
+
+        it('should return the list of walls, plus the new wall when a disabled wall is clicked', function () {
+            var clicked = [2,2];
+            expect(pathfinder.toggleWall([[0,0], [0,1]], clicked)).to.deep.equal([[0,0], [0,1], [2,2]]);
         });
-        describe('when a disabled wall is clicked', function () {
-            it('should return the list of walls, plus the new wall', function () {
-                var clicked = [2,2];
-                expect(pathfinder.toggleWall([[0,0], [0,1]], clicked)).to.deep.equal([[0,0], [0,1], [2,2]]);
-            });
-        })
+
+    });
+
+    describe('findNext', function () {
+        it('should return a list of points next to the start, given an empty grid', function () {
+            var state = { size: 3, walls: [], path: [] };
+            expect(pathfinder.findNext(state)).to.deep.equal([[0,0], [1,1], [0,2]]);
+        });
+    });
+
+    describe('getStart', function () {
+        it('should return the start when given a size', function () {
+            expect(pathfinder.getStart(3)).to.deep.equal([0, 1]);
+            expect(pathfinder.getStart(7)).to.deep.equal([0, 3]);
+            expect(pathfinder.getStart(11)).to.deep.equal([0, 5]);
+            expect(pathfinder.getStart(99)).to.deep.equal([0, 49]);
+        });
+    });
+
+    describe('getAdjacentFree', function () {
+        it('should return the filtered list of adjacent points', function () {
+            expect(pathfinder.getAdjacentFree([0,1], 3, [], [])).to.deep.equal([[0,0], [1,1], [0,2]]);
+            expect(pathfinder.getAdjacentFree([0,2], 3, [], [])).to.deep.equal([[0,1], [1,2]]);
+            expect(pathfinder.getAdjacentFree([1,1], 3, [], [])).to.deep.equal([[1,0], [2,1], [1,2], [0,1]]);
+            expect(pathfinder.getAdjacentFree([1,1], 3, [[1,0], [2,1], [1,2], [0,1]], [])).to.deep.equal([]);
+            expect(pathfinder.getAdjacentFree([1,1], 3, [], [[1,0], [2,1], [1,2], [0,1]])).to.deep.equal([]);
+            expect(pathfinder.getAdjacentFree([1,1], 3, [[1,0], [2,1]], [])).to.deep.equal([[1,2], [0,1]]);
+        });
+    });
+
+    describe('getAdjacent', function () {
+        it('should return the list of adjacent points', function () {
+            expect(pathfinder.getAdjacent([0,1])).to.deep.equal([[0,0], [1,1], [0,2], [-1,1]]);
+        });
+    });
+
+    describe('notOffGrid', function () {
+        it('should reject points off grid', function () {
+            expect(pathfinder.notOffGrid(3, [-1, 0])).to.equal(false);
+            expect(pathfinder.notOffGrid(3, [0, -1])).to.equal(false);
+
+            expect(pathfinder.notOffGrid(3, [-9, 0])).to.equal(false);
+            expect(pathfinder.notOffGrid(3, [0, -7])).to.equal(false);
+
+            expect(pathfinder.notOffGrid(3, [10, 0])).to.equal(false);
+            expect(pathfinder.notOffGrid(3, [0, 10])).to.equal(false);
+
+            expect(pathfinder.notOffGrid(10, [11, 0])).to.equal(false);
+            expect(pathfinder.notOffGrid(10, [0, 11])).to.equal(false);
+
+            expect(pathfinder.notOffGrid(10, [0, 0])).to.equal(true);
+            expect(pathfinder.notOffGrid(10, [1, 1])).to.equal(true);
+            expect(pathfinder.notOffGrid(10, [2, 2])).to.equal(true);
+        });
     })
+
+    describe('notX', function () {
+        it('should reject points which are in X', function () {
+            expect(pathfinder.notX([[1,1], [2,2], [3,3]], [1,1])).to.equal(false);
+            expect(pathfinder.notX([[1,1], [2,2], [3,3]], [2,2])).to.equal(false);
+
+            expect(pathfinder.notX([[1,1], [2,2], [3,3]], [1,2])).to.equal(true);
+            expect(pathfinder.notX([[1,1], [2,2], [3,3]], [4,2])).to.equal(true);
+            expect(pathfinder.notX([[1,1], [2,2], [3,3]], [2,7])).to.equal(true);
+        });
+    });
 
 });
